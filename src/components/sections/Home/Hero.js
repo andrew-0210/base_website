0d42c3e5581b0f67866437a9cd2aad23b262d7ce
Andrew Monthero <andrewmonthero21@gmail.com>
2025-02-10 16:23:15 +1000

    hero padding updated

--- src/components/sections/Home/Hero.js
+++ src/components/sections/Home/Hero.js
@@ -3,7 +3,9 @@ import React from "react";
 const Hero = () => {
   return (
     <section className="flex h-dvh">
-      <h1 className="flex items-end">Dream big, live simple.</h1>
+      <h1 className="flex items-end pb-[1rem] md:pb-[2.5rem]">
+        Dream big, live simple.
+      </h1>
     </section>
   );
 };
