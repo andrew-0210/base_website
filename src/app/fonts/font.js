981587619a099a20925eca9bba48a05bd9feaf8f
Andrew Monthero <andrewmonthero21@gmail.com>
2025-02-10 13:52:45 +1000

    Initial setup

---
+++ src/app/fonts/font.js
@@ -0,0 +1,30 @@
+import localFont from 'next/font/local';
+import { Inter } from 'next/font/google';
+
+export const ivy_presto = localFont({
+  src: [
+    {
+      path: './Ivy_Presto/ivy-presto-light.otf',
+      weight: '300',
+    },
+    {
+      path: './Ivy_Presto/ivy-presto-thin.otf',
+      weight: '400',
+    },
+    {
+      path: './Ivy_Presto/ivy-presto-headline.otf',
+      weight: '500',
+    },
+    {
+      path: './Ivy_Presto/ivy-presto-semi-bold.otf',
+      weight: '700',
+    },
+  ],
+  variable: '--font-ivy-presto',
+});
+
+export const inter = Inter({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
---
+++ src/components/layout/SmoothScrolling.js
@@ -0,0 +1,9 @@
+import { ReactLenis } from 'lenis/react';
+
+export default function SmoothScrolling({ children }) {
+  return (
+    <ReactLenis root options={{ duration: 1.2 }}>
+      {children}
+    </ReactLenis>
+  );
+}
