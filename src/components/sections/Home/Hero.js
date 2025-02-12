3589625789c9c9546150d1b49c8e7100a0cffd24
Andrew Monthero <andrewmonthero21@gmail.com>
2025-02-12 12:46:12 +1000

    opacity updated

--- src/components/sections/Home/Hero.js
+++ src/components/sections/Home/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
       <h1 className="z-[10] flex items-end px-[1.25rem] pb-[1rem] md:pb-[2.5rem]">
         Dream big, live simple.
       </h1>
-      <div className="absolute bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-transparent lg:h-dvh lg:w-dvw" />
+      <div className="absolute bg-gradient-to-t from-[rgba(0,0,0,0.6)] to-transparent lg:h-dvh lg:w-dvw" />
     </section>
   );
 };
