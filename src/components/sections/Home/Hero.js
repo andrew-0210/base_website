8df4e1982155f96a4ec1988e90fa7fcc2adf3c24
Andrew Monthero <andrewmonthero21@gmail.com>
2025-02-12 12:42:27 +1000

    hero height updated

--- src/components/sections/Home/Hero.js
+++ src/components/sections/Home/Hero.js
@@ -2,7 +2,7 @@ import HeroBackground from "@/components/ui/HeroBackground";
 
 const Hero = () => {
   return (
-    <section className="relative flex h-[80dvh] overflow-x-hidden lg:h-dvh">
+    <section className="relative flex h-[70dvh] overflow-x-hidden lg:h-dvh">
       <div className="absolute -z-30 h-full overflow-y-hidden lg:h-dvh lg:w-dvw">
         <HeroBackground />
       </div>
@@ -10,7 +10,7 @@ const Hero = () => {
       <h1 className="z-[10] flex items-end px-[1.25rem] pb-[1rem] md:pb-[2.5rem]">
         Dream big, live simple.
       </h1>
-      <div className="absolute bg-gradient-to-t from-[rgba(0,0,0,0.6)] to-transparent lg:h-dvh lg:w-dvw" />
+      <div className="absolute bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-transparent lg:h-dvh lg:w-dvw" />
     </section>
   );
 };
