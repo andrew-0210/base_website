b7bde210852ebe55f7809d87d7713762ec1f4c31
Andrew Monthero <166970322+andrew-0210@users.noreply.github.com>
2025-02-16 10:49:35 +0530

    Update SmoothScrolling.js

--- src/components/layout/SmoothScrolling.js
+++ src/components/layout/SmoothScrolling.js
@@ -2,7 +2,7 @@ import { ReactLenis } from "lenis/react";
 
 export default function SmoothScrolling({ children }) {
   return (
-    <ReactLenis root options={{ duration: 1.5 }}>
+    <ReactLenis root options={{ duration: 1.2 }}>
       {children}
     </ReactLenis>
   );
